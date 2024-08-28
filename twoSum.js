55df18d84dbb552b316fa94beafad40db4b54a7b
Fernando Suares <fernandoaqs@outlook.com.br>
2024-08-27 21:10:34 -0300

    add: twosum solution

--- twoSum.js
+++ twoSum.js
@@ -1,3 +1,9 @@
+/**
+ *          1. Two Sum
+ *              Given an array of integers nums and an integer target, return indices of the two numbers such that hey *              add up to target.
+*               You may assume that each input would have exactly one solution, and you may not use the same element *  *               twice.
+* */
+
 var twoSum = function(nums, target) {
     let hash = {};
     for(let i = 0; i < nums.length; i++){
