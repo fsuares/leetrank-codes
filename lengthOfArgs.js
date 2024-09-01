189e77e2ff59b98a5898aae62cfd926e245ad808
Fernando Suares <fernandoaqs@outlook.com.br>
2024-09-01 15:06:39 -0300

    add: 2703 - length of args solution

---
+++ lengthOfArgs.js
@@ -0,0 +1,9 @@
+/**
+ *          2703. Return Length of Arguments Passed
+ *              KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+ *              KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK.
+* */
+
+var argumentsLength = function(...args) {
+    return args.length;
+};
